db2016f3dbf51d72494cb24709b051bd6a630f31
VaiS <vaibhav.1807singh@gmail.com>
2023-05-05 03:27:29 +0530

    Friends Post

--- api/controllers/post.js
+++ api/controllers/post.js
@@ -1,9 +1,23 @@
 import {db} from "../connect.js";
+import jwt  from "jsonwebtoken";
 
 export const getPosts = (req,res)=>{
-     const q = `SELECT p.*,u.id AS userId,name,profilePic FROM posts AS p JOIN users AS u ON (u.id = p.userId)`
-     db.query(q,(err,data)=>{
+
+     const token = req.cookies.accessToken;
+     if(!token) return res.status(401).json("Not logged in");
+
+   jwt.verify(token,"secretkey",(err,userInfo) => {
+      if(err) return res.status(403).json("Token is not valid");
+   
+     const q = `SELECT p.*,u.id AS userId,name,profilePic FROM posts AS p JOIN users AS u ON (u.id = p.userId)
+      JOIN relationships AS r ON (p.userId = r.followedUserId AND r.followerUserId= ?)
+      ORDER BY p.createdAt DESC
+      `;
+
+     db.query(q,[userInfo.id],(err,data)=>{
+      
         if(err) return res.status(500).json(err);
         return res.status(200).json(data); 
-     })
-}
+     });
+   });
+};
